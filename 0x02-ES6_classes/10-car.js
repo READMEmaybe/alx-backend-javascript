8c45fdb359549cd6a9b87a3c15513e3cc70629eb
READMEmaybe <haroune11411@gmail.com>
2024-04-12 00:43:02 +0000

    Add task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,44 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  set brand(brand) {
+    if (typeof brand !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    this._brand = brand;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set motor(motor) {
+    if (typeof motor !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    this._motor = motor;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set color(color) {
+    if (typeof color !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+    this._color = color;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  cloneCar() {
+    return new this.constructor();
+  }
+}
