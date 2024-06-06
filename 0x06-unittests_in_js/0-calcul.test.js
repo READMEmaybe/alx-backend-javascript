48dd6ed7df00606ad63be0bc68c7bcf63a82785c
READMEmaybe <haroune11411@gmail.com>
2024-06-06 18:03:57 +0100

    Fix task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,9 @@ const assert = require('assert');
 
 describe('calculateNumber', () => {
   it('should round a and b and return the sum', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
